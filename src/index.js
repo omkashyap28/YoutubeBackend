526e41b6efef13d46ca6c8d25792c90ff60ad670
Hariom Kashyap <omkashyap6870@gmail.com>
2025-12-14 12:37:35 +0530

    setting up required files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello Backend");
+const express = require("express");
+require("dotenv").config({
+  path: ".env",
+});
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", function (req, res) {
+  res.send("HELLO BACKEND");
+});
+
+app.listen(port, function () {
+  console.log("App id running on ", port);
+});
